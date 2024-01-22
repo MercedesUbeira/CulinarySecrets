af268feb4fa86430bb311286057ae83e0159f769
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2024-01-22 14:12:24 -0300

    Js - header and footer
    Added js file and applied header and footer to all the pages.

---
+++ commonjs.js
@@ -0,0 +1,30 @@
+//Since all the pages have header and footer, I figured that it would be easier to use to have one file for header, one for footer and one js that can be applied to all pages.
+
+
+function includeHTML(filePath, targetId) {
+    fetch(filePath)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to fetch ${filePath}: ${response.statusText}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(targetId).innerHTML = data;
+        })
+        .catch(error => console.error(error));
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    includeHTML("header.html", "header");
+    includeHTML("footer.html", "footer");
+});
+
+//Function for the "Begin to Savor" btn that scrolls down to the categories.
+
+function scrollToContent() {
+    window.scrollTo({
+        top: document.getElementById('content').offsetTop,
+        behavior: 'smooth'
+    });
+}
